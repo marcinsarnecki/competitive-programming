325d7829ed846552902d2a9356a0105f38632e11
Marcin <marcin.sarnecki44@gmail.com>
2021-04-15 16:07:25 +0200

    Update tes.cpp

--- OI/XVII/TES/tes.cpp
+++ OI/XVII/TES/tes.cpp
@@ -1,71 +1,57 @@
 #include<bits/stdc++.h>
-
+#define f first
+#define s second
+#define pb push_back
+#define rep(i,a,b) for(int i = a; i <= b; i++)
+#define repv(i,a,b) for(int i = a; i >= b; i--)
 using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
 
 #define N 1000001
-#define INF 1000010
+#define INF 10000000
 vector<int> wystapienia[N];
-int tab[N];
-int z[N];
+int tab[N], n, m, ile, granica, b[N];
 
-int m,n,l,granica,odp;
-
-int nastepny(int value,int g) //funckja zwraca pierwsza pozycje value wieksza niz g
-{
-	if(wystapienia[value].size()==0) return INF;
-	int lewo = 0,prawo = wystapienia[value].size()-1;
-	int mid = (lewo+prawo)/2;
-	while(!(wystapienia[value][mid]<=g && wystapienia[value][mid+1] > g))
-	{
-		if(wystapienia[value][mid]<g) lewo = mid+1;
-		else prawo = mid;
-		mid = (lewo + prawo)/2;
+int nastepny(int num, int g) {//w liczbie 'num' pierwsze wystapienie na pozycji wiekszej niz 'g'
+	if(g == INF)
+		return INF;
+	if(wystapienia[num].size()==0) //jesli tej liczby wgl nie ma to zwracamy INF 
+		return INF; 
+	int l = 0, r = wystapienia[num].size() - 1, mid;
+	while(l <= r) { //binsearch
+		mid = (l + r) / 2;
+		if(wystapienia[num][mid] > g)
+			r = mid - 1;
+		else
+			l = mid + 1;
 	}
-	return wystapienia[value][mid+1];
+	return wystapienia[num][r+1];
 }
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
-	
 	cin>>m;
-	for(int i = 1; i <= m; i++) 
-	{
+	rep(i,1,m) {
 		cin>>tab[i];
-		if(wystapienia[tab[i]].size()==0) wystapienia[tab[i]].push_back(-1);
-		wystapienia[tab[i]].push_back(i);
+		if(wystapienia[tab[i]].size() == 0) //dla kazdej liczby ktora sie pojawi na wejsciu dodajemy na poczatku -1 do wystapien
+			wystapienia[tab[i]].pb(-1);		
+		wystapienia[tab[i]].pb(i);
 	}
-	for(int i = 1; i <= m; i++) wystapienia[tab[i]].push_back(INF);
-	
-	
-	cin>>n;
-	for(int j = 1; j <= n; j++)
-	{
-		cin>>l;
+	rep(i,1,m)
+		wystapienia[tab[i]].pb(INF);//INF na koniec vectora z wystapieniami dla kazdej liczby ktora pojawila sie na wejsciu
+	  cin>>n;
+	rep(i,1,n) {
+		cin>>ile;
 		granica = 0;
-		odp = 1;
-		for(int i = 1; i <= l; i++)
-		{
-			cin>>z[i];
-		}
-		for(int i = 1; i <= l; i++)
-		{
-			granica = nastepny(z[i],granica);
-			if(granica==INF) 
-			{
-				odp = 0;
-			}
-			if(odp == 0) break;
-		}
-		if(odp) cout<<"TAK\n";
-		else cout<<"NIE\n";
-		
-		
+		rep(j,1,ile)
+			cin>>b[j];
+		rep(j,1,ile) //granica oznacza w ktorym miejscu skonczylismy, i tak dla kazdej kolejnej liczby obliczamy jej pierwsze wystapienie wieksze niz granica
+			granica = nastepny(b[j], granica);
+		if(granica != INF) //jesli nie wyladowalismy w nieskonczonosci to znaczy ze odpowiedz istnieje
+			cout<<"TAK\n";
+		else
+			cout<<"NIE\n";
 	}
-	
-	
-	
-	
-	
 	return 0;
 }
