bf3bf04dfdb67727312802fd0ffdc0bc69c28e7a
Marcin <marcin.sarnecki44@gmail.com>
2021-09-10 20:19:42 +0200

    Create new-file-template.cpp

---
+++ useful-things/new-file-template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+#define rep(i,a,b) for(int i = a; i <= b; i++)
+#define repv(i,a,b) for(int i = a; i >= b; i--)
+using namespace std;
+typedef pair<int,int> pii;
+typedef long long ll;
+
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	
+	return 0;
+}
