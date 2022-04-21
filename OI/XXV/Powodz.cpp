75950e8e6bddf26240ef5938660233d194be5c7d
Marcin <marcin.sarnecki44@gmail.com>
2022-04-21 14:45:18 +0200

    Update Powodz.cpp

--- OI/XXV/Powodz.cpp
+++ OI/XXV/Powodz.cpp
@@ -2,109 +2,110 @@
 #define f first
 #define s second
 #define pb push_back
+#define rep(i,a,b) for(int i = a; i <= b; i++)
+#define repv(i,a,b) for(int i = a; i >= b; i--)
 using namespace std;
-typedef pair<int,int> pii ;
+typedef pair<int,int> pii;
 typedef long long ll;
 
-long long Ans = 1, mod = 1e9+7, n, m, H;
+ll Ans = 1, mod = 1e9+7, n, m, H;
 
-struct skladowa{
-	long long ans, h;
+struct node{
+	ll ans, h;
 	pii parent;
-	int rank;
+	int size;
 };
-vector<vector<skladowa>> pole;
+vector<vector<node>> field;
 
-struct kraw{
+struct edge{
 	int x1, y1, x2, y2, h;
-	kraw(int a, int b, int c, int d, int e) {
-		x1 = a;
-		y1 = b;
-		x2 = c;
-		y2 = d;
-		h = e;
+	edge(int a, int b, int c, int d, int e){
+		x1 = a;                                                                 
+        y1 = b;                                                                 
+        x2 = c;                                                                 
+        y2 = d;                                                                 
+        h = e;
 	}
-	kraw();
+	edge();
 };
-vector<kraw> kr;
-bool compare(const kraw &k1, const kraw &k2) {
-	return k1.h < k2.h;
+vector<edge> edges;
+
+bool compare(const edge &e1, const edge &e2) {
+	return e1.h < e2.h;
 }
 
-void wczytaj() {
+void input() {
 	cin>>n>>m>>H;
 	if(m > 1)
-		for(int i = 1; i <= n; i++) 
-			for(int j = 1; j < m; j++) {
-				int h;
-				cin>>h;
-				kr.pb(kraw(j,i,j+1,i,h));
+		rep(i,1,n)
+			rep(j,1,m-1) {
+				int h; cin>>h;
+				edges.pb(edge(j,i,j+1,i,h));
 		}
 	if(n > 1)
-		for(int i = 1; i < n; i++)
-			for(int j = 1; j <= m; j++) {
-				int h;
-				cin>>h;
-				kr.pb(kraw(j,i,j,i+1,h));
+		rep(i,1,n-1)
+			rep(j,1,m) {
+				int h; cin>>h;
+				edges.pb(edge(j,i,j,i+1,h));
 			}
 }
 
 void init() {
-	sort(kr.begin(),kr.end(),compare);
-	pole.resize(n+1);
-	for(int i = 1; i <= n; i++) pole[i].resize(m+1);
-	for(int i = 1; i <= n; i++)
-		for(int j = 1; j <= m; j++) {
-			pole[i][j].ans = 1;
-			pole[i][j].h = 0;
-			pole[i][j].parent = {i,j};
+	sort(edges.begin(),edges.end(),compare);
+	field.resize(n+1);
+	rep(i,1,n) //dynamiczne utworzenie pola o rozmiarze n * m
+		field[i].resize(m+1);
+	rep(i,1,n)
+			rep(j,1,m) { 
+			field[i][j].ans = 1;//w kazdym polu poczatkowo mozna ustawic wode na 1 sposob 
+			field[i][j].h = 0;//do wysokosci 0
+			field[i][j].parent = {i,j};//find union poczatek
+			field[i][j].size = 1;
 		}
 }
 
 pii find(pii v) {
-	if(v == pole[v.f][v.s].parent) 
+	if(v == field[v.f][v.s].parent) 
 		return v;
-	return pole[v.f][v.s].parent = find(pole[v.f][v.s].parent);
+	return field[v.f][v.s].parent = find(field[v.f][v.s].parent);
 }
 
 void union_sets(pii v, pii u, int h) {
 	pii a = find(v), b = find(u);
 	if(a != b) {
-		if(pole[a.f][a.s].rank < pole[b.f][b.s].rank)
+		if(field[b.f][b.s].size < field[a.f][a.s].size)
 			swap(a,b);
-		pole[b.f][b.s].parent = a;
+		field[a.f][a.s].parent = b;//dolaczamy (mniejsze) a do (wiekszego) b
+		field[b.f][b.s].size += field[a.f][a.s].size;
 		
-		long long ans1 = pole[a.f][a.s].ans, ans2 = pole[b.f][b.s].ans;
-		long long dif1 = h - pole[a.f][a.s].h, dif2 = h - pole[b.f][b.s].h;
-		pole[a.f][a.s].ans = (ans1 + dif1) * (ans2 + dif2) % mod;
-		pole[a.f][a.s].h = h;	
-		if(pole[a.f][a.s].rank == pole[b.f][b.s].rank)
-			pole[a.f][a.s].rank++;
+		ll ans1 = field[a.f][a.s].ans, ans2 = field[b.f][b.s].ans;
+		ll dif1 = h - field[a.f][a.s].h, dif2 = h - field[b.f][b.s].h;
+		field[b.f][b.s].ans = (ans1 + dif1) * (ans2 + dif2) % mod;//liczba sposobow ustawienia wody w nowym polaczonym obszarze
+		field[b.f][b.s].h = h;
 	}
 }
 
 void mst() {
-	for(auto it: kr) {
+	for(auto it: edges) {//kruskal
 		pii a = {it.y1,it.x1}, b = {it.y2,it.x2};
 		int h = it.h;
 		union_sets(a,b,h);
 	}
 }
 
-void wypisz_wynik() {
+void print() {
 	pii v = find({1,1});
-	long long h = pole[v.f][v.s].h;
-	Ans = pole[v.f][v.s].ans;
-	Ans += H - h;
-	cout<<Ans % mod;
+	ll h = field[v.f][v.s].h;
+	Ans = field[v.f][v.s].ans;
+	Ans += H - h;//sciana dokola calych danych
+	cout<<Ans % mod<<"\n";
 }
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(0);
-	wczytaj();
+	input();
 	init();	
 	mst();
-	wypisz_wynik();
+	print();
 	return 0;
 }
